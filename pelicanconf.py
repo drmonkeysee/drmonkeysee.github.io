2da915624d8d7f82a8df2d18a0c438c606b53c65
Brandon Stansbury <brandonrstansbury@gmail.com>
2015-12-04 20:11:13 -0800

    whole lotta setting changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,8 +3,8 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Dr Monkeysee'
-SITENAME = 'Code Monkey'
-SITEURL = ''
+SITENAME = 'Dev Log and also Other Blog Stuff too'
+SITEURL = 'http://drmonkeysee.github.io'
 PATH = 'content'
 TIMEZONE = 'America/Los_Angeles'
 DEFAULT_LANG = 'en'
@@ -19,23 +19,29 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/drmonkeysee'),
+          ('GitHub', 'https://github.com/drmonkeysee'),
+          ('Email', 'mailto:drmonkeysee@gmail.com'))
 
+GITHUB_URL = 'https://github.com/drmonkeysee'
+TWITTER_USERNAME = 'drmonkeysee'
 DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+THEME = 'notmyidea'
+DISPLAY_CATEGORIES_ON_MENU = False
+RELATIVE_URLS = True
 
 # Url stuff
 DEFAULT_DATE = 'fs'
 SLUGIFY_SOURCE = 'basename'
 ARTICLE_URL = '{slug}'
 ARTICLE_SAVE_AS = '{slug}/index.html'
+TAG_URL = '{slug}'
+TAG_SAVE_AS = '{slug}/index.html'
+CATEGORY_URL = '{slug}'
+CATEGORY_SAVE_AS = '{slug}/index.html'
 # the theme still includes and links to author, it's just a blank url
 AUTHOR_URL = ''
 AUTHOR_SAVE_AS = ''
